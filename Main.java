4aea62855dd3c3911b1223559882038e60dcf41f
ajit51 <ajitk433@gmail.com>
2023-12-13 10:52:40 +0530

    added new statement

--- Main.java
+++ Main.java
@@ -6,5 +6,7 @@ public class Main{
         System.out.println("output from java program");
         System.out.println(new Date());
         System.out.println("Added new statement");
+        Student student = new Student();
+        student.sayHello();
     }
 }
---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    public Student(){
+        System.out.println("Creating new Student object");
+    }
+
+    public void sayHello(){
+        System.out.println("Hi, I am a Student");
+    }
+}
